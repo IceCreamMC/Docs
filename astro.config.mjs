b5b571a59a6bb1073206ecd53bc81189391f346f
James Smith <eesastudios@gmail.com>
2024-09-07 00:16:10 +0000

    some pages ported over

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from '@astrojs/mdx';
+
+import vue from '@astrojs/vue';
+
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'IceCreamMC Docs',
+        social: {
+            github: 'https://github.com/IceCreamMC',
+            discord: 'https://discord.icecreammc.xyz',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Installation', slug: 'guides/installation' },
+                    { label: 'Migration', slug: 'guides/migration' },
+                    { label: 'Requirements', slug: 'guides/requirements'}
+                ],
+            },
+        ],
+        }), mdx(), vue(), react(), tailwind(), robotsTxt()],
 });
